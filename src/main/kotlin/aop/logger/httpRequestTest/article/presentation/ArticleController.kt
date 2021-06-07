e1347378376b866dd9be7d349aad8979a701fca0
flamme <flamme1004@gmail.com>
2021-06-07 20:37:57 +0900

    log 및 logback 설정

--- src/main/kotlin/aop/logger/httpRequestTest/article/presentation/ArticleController.kt
+++ src/main/kotlin/aop/logger/httpRequestTest/article/presentation/ArticleController.kt
@@ -1,12 +1,25 @@
 package aop.logger.httpRequestTest.article.presentation
 
 import aop.logger.httpRequestTest.article.service.ArticleService
+import mu.KLogging
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class ArticleController(private val articleService :ArticleService) {
+
+    companion object : KLogging()
+
+    @GetMapping("/log")
+    fun callLogRainBow() {
+        logger.trace("TRACE LOG")
+        logger.debug("DEBUG LOG")
+        logger.info("INFO LOG")
+        logger.warn("WARN LOG")
+        logger.error("ERROR LOG")
+    }
 
     @PostMapping("/article")
     fun callArticleInfo(@RequestBody request : Map<String, Any>)  = articleService.getArticleList()
